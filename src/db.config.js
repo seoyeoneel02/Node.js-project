16e212153dca23294f24ba52e8da3cfd42371987
seoyeoneel02 <seoyeoneel02@gmail.com>
2024-10-20 00:40:22 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/db.config.js
@@ -0,0 +1,15 @@
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
---
+++ src/index.js
@@ -0,0 +1,83 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import swaggerAutogen from "swagger-autogen";
+import swaggerUiExpress from "swagger-ui-express";
+
+
+dotenv.config();
+
+const app = express()
+const port = process.env.PORT;
+
+app.use((req, res, next) => {
+    res.success = (success) => {
+      return res.json({ resultType: "SUCCESS", error: null, success });
+    };
+  
+    res.error = ({ errorCode = "unknown", reason = null, data = null }) => {
+      return res.json({
+        resultType: "FAIL",
+        error: { errorCode, reason, data },
+        success: null,
+      });
+    };
+  
+    next();
+  });
+
+app.use(cors());
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+    "/docs",
+    swaggerUiExpress.serve,
+    swaggerUiExpress.setup({}, {
+      swaggerOptions: {
+        url: "/openapi.json",
+      },
+    })
+  );
+  
+  app.get("/openapi.json", async (req, res, next) => {
+    // #swagger.ignore = true
+    const options = {
+      openapi: "3.0.0",
+      disableLogs: true,
+      writeOutputFile: false,
+    };
+    const outputFile = "/dev/null";
+    const routes = ["./src/index.js"];
+    const doc = {
+      info: {
+        title: "Node.js 개인 프로젝트",
+        description: "Node.js 개인 프로젝트입니다."
+      },
+      host: "localhost:3000"
+    };
+  
+    const result = await swaggerAutogen(options)(outputFile, routes, doc);
+    res.json(result ? result.data : null);
+  });
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.use((err, req, res, next) => {
+    if (res.headersSent) {
+      return next(err);
+    }
+  
+    res.status(err.statusCode || 500).error({
+      errorCode: err.errorCode || "unknown",
+      reason: err.reason || err.message || null,
+      data: err.data || null,
+    });
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
